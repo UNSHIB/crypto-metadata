d827d15fd6dd34ba85a43f989419a663eb6bad21
Thang Tran <thangtp@gmail.com>
2021-11-04 12:25:44 -0700

    generate meta.json

--- index.py
+++ index.py
@@ -25,19 +25,28 @@ def generate_indexes():
             id, name, symbol, _, _ = c
             f.write(f'|{idx+1}|[{symbol.upper()}](./coins/{id[0]}/{id}.json)|{name}|\n')
 
-    with open('./github_orgs.txt', 'w') as f:
-        visited = {}
-        for c in coins:
-            github_org = c[-1]
 
-            if github_org and github_org not in visited:
-                visited[github_org] = True
-                f.write(f'{github_org}\n')
+    orgs = {}
+    for c in coins:
+        id, name, symbol, _, github_org = c
+        if github_org:
+            github_org = github_org.lower()
+
+            if github_org not in orgs:
+                orgs[github_org] = dict(
+                    id=id,
+                    name=name,
+                    symbol=symbol.upper()
+                )
 
     with open('./coins/meta.json', 'w') as f:
-        f.write(json.dumps({
-            "total_cryptos": len(coins)
-        }))
+        f.write(
+            json.dumps(
+                dict(
+                    total_cryptos=len(coins),
+                    orgs=orgs
+                ),
+                indent=4))
 
 if __name__ == '__main__':
     generate_indexes()
